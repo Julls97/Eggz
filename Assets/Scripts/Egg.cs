fb16e29cb2528ce9569785d20f213c4610f1fb46
Jull <yulia.jolie@yandex.ru>
2021-06-11 13:44:03 +0500

    new tragectory

--- Assets/Scripts/Egg.cs
+++ Assets/Scripts/Egg.cs
@@ -1,27 +1,55 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Egg : MonoBehaviour {
 	[SerializeField] private float powerForce = 5f; // сила толчка 
 	[SerializeField] private SpriteRenderer sprite;
 	[SerializeField] private Rigidbody2D myRigidbody2D;
 	[SerializeField] private Collider2D myCollider2D;
+	[SerializeField] private Trajectory trajectory;
 
 	private float bigSize = 2f;
 	private float maxAngle = 6;
 	private float posUp = 0.6f;
 	private float posRight = 0.4f;
 	private Vector3 startPos;
-	private Coroutine bigSizeCo;
+	private Coroutine bigSizeCo, moveCo;
+	
+	public bool hasClicked = false;
+	
 	private void Awake(){
 		startPos = transform.position;
 		
-		// Time.timeScale = 0.3f;
+		// Time.timeScale = 0.5f;
 	}
 
-	private void AddForce(int mult = 1){
-		myRigidbody2D.AddForce(transform.up * powerForce * mult, ForceMode2D.Impulse);
+	private void AddForce(float mult = 1){
+		trajectory.SetPoints(transform.position, transform.up * powerForce * mult);
+		trajectory.ShowTrajectory();
+		
+		if (moveCo != null) StopCoroutine(moveCo);
+		moveCo = StartCoroutine(Move());
+		
+		// myRigidbody2D.AddForce(transform.up * powerForce * mult, ForceMode2D.Impulse);
+	}
+
+	private IEnumerator Move() {
+		var points = trajectory.GetPoints();
+		// Utility.RemoveNear(points);
+		
+		var speedMove = 0.4f;
+		
+		foreach (var point in points) {
+			var t = 0f;
+
+			while (t < 0.3f) {
+				t += Time.deltaTime * speedMove;
+				transform.position = Vector3.Lerp(transform.position, point, t);
+				yield return null;
+			}
+		}
 	}
 
 	public void SetBigSize(){
@@ -63,16 +91,18 @@ public class Egg : MonoBehaviour {
 		// transform.rotation = rot;
 		transform.eulerAngles = new Vector3(0, 0, 0);
 
+		hasClicked = false;
+		
 		Play();
 		AddForce(1);
 	}
 
 	public void Play(){
-		myRigidbody2D.simulated = true;
+		// myRigidbody2D.simulated = true;
 	}
 
 	public void Stop(){
-		myRigidbody2D.simulated = false;
+		// myRigidbody2D.simulated = false;
 	}
 
 	private void OnTriggerEnter2D(Collider2D other){
@@ -80,27 +110,31 @@ public class Egg : MonoBehaviour {
 		else if (other.CompareTag($"circle2")) Controller.Instance.AddScore(2);
 		else if (other.CompareTag($"circle4")) Controller.Instance.AddScore(4);
 		else if (other.CompareTag($"dead zone")) {
-			Controller.Instance.GameOver();
 			print("dead Zone");
+			Controller.Instance.GameOver();
 		}
 	}
 
-	private bool hasClicked = false;
-
 	private void Update(){
 		if (transform.position.y > -1.0f) hasClicked = false;
-		
-		if (transform.position.y < -10.0f) Controller.Instance.GameOver();
+
+		if (transform.position.y < -6.0f) Controller.Instance.GameOver();
 	}
 
 	private void OnMouseDown(){
-		if (hasClicked) return;
+		print("OnMouseDown");
 		
-		if (transform.position.y < -1.0f) {
+		if (hasClicked) {
+			print("return. hasClicked = " + hasClicked);
+			return;
+		}
+		
+		if (transform.position.y < -2.0f) { // -1
+			Stop();
 			hasClicked = true;
 			var mouse = Input.mousePosition;
 			
-			Vector2 screenPosition = new Vector2(mouse.x, mouse.y);
+			var screenPosition = new Vector2(mouse.x, mouse.y);
 			Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
 
 			var point = new GameObject("point");
@@ -124,26 +158,20 @@ public class Egg : MonoBehaviour {
 				angle = x * maxAngle * -1;
 			}
 			else if (locPos.x > 0 && locPos.y < 0 ) { // 4
-				angle = x * maxAngle * -1;
+				angle = x * maxAngle;
 			}
 			else if (locPos.x < 0 && locPos.y > 0 ) { // 2
 				angle = x * maxAngle;
 			}
 			else if (locPos.x < 0 && locPos.y < 0 ) { // 3
-				angle = x * maxAngle;
+				angle = x * maxAngle * -1;
 			}
 			
 			transform.Rotate(new Vector3(0, 0, angle));
 			print(angle);
 			
-			AddForce(3);
+			AddForce(1f);
+			Play();
 		}
 	}
-
-	// private void Update(){
-	// 	if (Input.GetMouseButtonDown(0)) {
-	// 		// if (inZone)
-	// 		
-	// 	}
-	// }
 }
