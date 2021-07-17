a1fa755ce322940d15b3b9f93f30454fb4547c42
Rus_HomePC <rispekt.196@gmail.com>
2021-07-17 15:07:20 +0500

    FinalUpdate. - Egg out of bounds view. - Multple egg support (should work) - Bounciness and stuff

--- Assets/Develop/Loper/NewEggController/Scripts/EggController.cs
+++ Assets/Develop/Loper/NewEggController/Scripts/EggController.cs
@@ -10,27 +10,52 @@ namespace MainGame.Egg
         [SerializeField] Rigidbody2D eggRigitbody;
         [SerializeField] float forceMult = 10;
         [SerializeField] Vector2 randomForceAngle = new Vector2(-30, 30);
-        [SerializeField] Vector2 endPoint =Vector2.zero;
+        [SerializeField] Vector2 endPoint = Vector2.zero;
         [SerializeField] float gravityScale = 0.5f;
-        [SerializeField] EggOutOfBoundsView outofBoundsView;
-        public UnityAction OnEggAppearOnSceen { get; set; }
-        public UnityAction OnEggDisappearOnSceen { get; set; }
+        [SerializeField] EggOutOfBoundsView outOfBoundsView;
+        [SerializeField] Vector2 addRotationRange = new Vector2(-30, 30);
+        public bool AllowTouch { get; set; } = true;
+        UnityAction<Collider2D> onTriggerEnter;
+        UnityAction<Collider2D> onTriggerExit;
 
         private void OnBecameInvisible()
         {
-            outofBoundsView.EnableView(true);
+            outOfBoundsView.gameObject.SetActive(true);
         }
         private void OnBecameVisible()
         {
-            outofBoundsView.EnableView(false);
+            outOfBoundsView.gameObject.SetActive(false);
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            onTriggerEnter.Invoke(collision);
+        }
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            onTriggerExit.Invoke(collision);
+        }
+        private void OnDestroy()
+        {
+            OutOfBoundsViewManager.instance.DisposeView(outOfBoundsView);
         }
         bool locked = true;
         private void OnMouseDown()
         {
-            UnlockEgg();
-            ResetForce();
-            Push();
-            Debug.Log("Pressed");
+            if (AllowTouch)
+            {
+                AllowTouch = false;
+                UnlockEgg();
+                ResetForce();
+                Push();
+                Debug.Log("Pressed");
+            }
+        }
+        public void SetUp(UnityAction<Collider2D> onTriggerEnter, UnityAction<Collider2D> onTriggerExit)
+        {
+            this.onTriggerEnter = onTriggerEnter;
+            this.onTriggerExit = onTriggerExit;
+            outOfBoundsView = OutOfBoundsViewManager.instance.GetViewObj();
+            outOfBoundsView.TrackedEgg = this.transform;
         }
         public void UnlockEgg()
         {
@@ -46,8 +71,9 @@ namespace MainGame.Egg
         public void Push()
         {
             Vector2 force = Utility.GetPointInCircle(Vector2.zero, 1, Utility.RandomRangeVector(randomForceAngle));
-            Debug.Log(force);
-            Vector2 totalForce = new Vector2(force.x * forceMult,  forceMult);
+            Vector2 totalForce = new Vector2(force.x * forceMult, forceMult);
+            float rotation = Utility.RandomRangeVector(randomForceAngle);
+            eggRigitbody.angularVelocity += rotation;
             eggRigitbody.AddForce(totalForce);
         }
         private void Update()
--- Assets/Develop/Loper/NewEggController/Scripts/GameManager.cs
+++ Assets/Develop/Loper/NewEggController/Scripts/GameManager.cs
@@ -2,10 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MainGame
+namespace MainGame.Egg
 {
     public class GameManager : MonoBehaviour
     {
-   
+        public static GameManager instance;
+        [SerializeField] GameObject menuScreen;
+        [SerializeField] GameObject gameHolder;
+        [SerializeField] EggManager eggManager;
+
+        private void Awake()
+        {
+            if (instance != null)
+                Destroy(instance);
+            instance = this;
+        }
+        private void Start()
+        {
+            EnableMainMenu();
+        }
+        public void EnableMainMenu()
+        {
+            menuScreen.SetActive(true);
+        }
+        public void LaunchGame()
+        {
+            menuScreen.SetActive(false);
+            eggManager.LaunchGame();
+        }
+        public void GameOver()
+        {
+            EnableMainMenu();
+        }
     }
 }
